7b146475e2cd8d8117651e59b0d385730facb4e7
ergodique <124272650+ergodique@users.noreply.github.com>
2023-02-06 11:28:34 +0300

    Empty file corrected

--- CondinGame Java/findSumPair.java
+++ CondinGame Java/findSumPair.java
@@ -1,5 +1,27 @@
-class Scratch {
+import java.util.Arrays;
+
+public class Main {
+    public static int[] findSumPair(int[] numbers,int k){
+        int[] result={0,0};
+        boolean found=false;
+        for (int i=0;i< numbers.length;i++){
+            for (int j=i+1;j< numbers.length;j++){
+                if(numbers[i]+numbers[j]==k){
+                    result[0]=i;
+                    result[1]=j;
+                    found=true;
+                    break;
+                }
+            }
+            if (found){
+                break;
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        
+        int[] numbers={1,1,2,9,4,13,0};
+        int k=13;
+        System.out.println(Arrays.toString(findSumPair(numbers,k)));
     }
 }
